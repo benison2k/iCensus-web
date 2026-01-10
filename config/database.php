ed419b471cbe076dd6690bb23b5aa27832b43709
benison2k <jeremybensonlastrollo@gmail.com>
2026-01-10 19:40:25 +0800

    Update database.php
    -removed hardcoded credentials

--- config/database.php
+++ config/database.php
@@ -1,8 +1,10 @@
 <?php
+// config/database.php
+
 return [
-    'host' => 'localhost',
-    'dbname' => 'u746374185_icensus',
-    'user' => 'u746374185_icensus',      // default XAMPP user
-    'password' => 'Jeben2263',      // default XAMPP password is empty
+    'host' => $_ENV['DB_HOST'] ?? 'localhost',
+    'dbname' => $_ENV['DB_NAME'] ?? 'icensus_db',
+    'user' => $_ENV['DB_USER'] ?? 'root',
+    'password' => $_ENV['DB_PASS'] ?? '',
     'charset' => 'utf8mb4'
 ];
